0dde291f04bef8762c1fbde313441995720a9b12
taidalab <zdajun1988@163.com>
2017-05-06 09:01:55 +0800

    add origin lib

---
+++ linux-335x-T/include/generated/compile.h
@@ -0,0 +1,6 @@
+/* This file is auto generated, version 2 */
+#define UTS_MACHINE "arm"
+#define UTS_VERSION "#2 Thu Aug 11 10:57:53 CST 2016"
+#define LINUX_COMPILE_BY "root"
+#define LINUX_COMPILE_HOST "zwj"
+#define LINUX_COMPILER "gcc version 4.5.2 (Sourcery G++ Lite 2011.03-41) "
